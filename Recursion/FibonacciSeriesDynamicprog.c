5e1de860a3ed1a7ff43700e95b78d23d25e18ec1
kevin909cty <cheungkevinviola909@gmail.com>
2019-07-14 16:03:39 +0800

    updated on the comment

--- Recursion/FibonacciSeriesDynamicprog.c
+++ Recursion/FibonacciSeriesDynamicprog.c
@@ -1,17 +1,17 @@
 /*
-=========================================================================================================
-About_this_code: Further Improve on the fionacci series by using recursion but in a dynamic programming.
-			 It is the solution to solve the problem of excessive recursion, from the time complexity of 
-			 O(2^n) to O(n).
+======================================================================================================================
+About_this_code: 	Further Improve on the fionacci series by using recursion but in a dynamic programming.
+			 It is the solution to solve the problem of excessive recursion, from the time 
+			 complexity of O(2^n) to O(n).
 Compiler: 		 GCC, GNU Compiler Collection, "gcc -o compilefile FibonacciSeriesDynamicprog.c"
 Author: 		 Cheung Tsz Yin
 Github:			 kevin909cty
 Date:   		 14 / 07 / 2019
 Comment: 		 Please feel free to contact me if there are any mistake or code that 
 			 can be further improve. Thank you very much! ^_^ 
-Contact email: 	 cheungkevinviola909@gmail.com
+Contact email: 	 	cheungkevinviola909@gmail.com
 p.s.:			 This code only works where "term <= 46". Beyond 46 will return a wrong value.
-=========================================================================================================
+======================================================================================================================
 */
 #include <stdio.h>
 #include <stdlib.h>
