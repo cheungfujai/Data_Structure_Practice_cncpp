0304f0e94a15cf3946cd5b2aa9e858286819d2b0
kevin909cty <cheungkevinviola909@gmail.com>
2019-07-18 23:44:05 +0800

    finally done ncr

--- Recursion/nCr.c
+++ Recursion/nCr.c
@@ -1,6 +1,7 @@
 /*
-======================================================================================================================
-About_this_code: 	My self-exercise to implement the nCr (Pascal Triangle) series by using recursion.
+===========================================================================================================================
+About_this_code: 	My self-exercise to implement the nCr (Pascal Triangle) series by using recursion under the practice of
+				 dynamic programming.
 Compiler: 		 GCC, GNU Compiler Collection, "gcc -o compilefile nCr.c"
 Author: 		 Cheung Tsz Yin
 Github:			 kevin909cty
@@ -8,7 +9,7 @@ Date:   		 18 / 07 / 2019
 Comment: 		 Please feel free to contact me if there are any mistake or code that 
 			 can be further improve. Thank you very much! ^_^ 
 Contact email: 	 	 cheungkevinviola909@gmail.com
-======================================================================================================================
+===========================================================================================================================
 */
 
 // On_Progress
@@ -16,37 +17,30 @@ Contact email: 	 	 cheungkevinviola909@gmail.com
 #include <stdio.h>
 #include <stdlib.h>
 
-int ncr(int n, int r, int*** ptr);
+int ncr(int n, int r);
 
 int main(){
-	int n = 10, r = 6;
-	int** ptr = malloc(n * sizeof(int));
-	for (int i = 0; i < n; i++){
-		ptr[i] = malloc((i + 1) * sizeof(int));
-		for (int j = 0; j < i + 1; j++){
-			ptr[i][j] = -1;
-		}
-	}
-	printf("The value of '%d' C '%d' = %d.\n", n, r, ncr(n, r, &ptr));
-	free(ptr);
+	int n = 20, r = 10;
+	printf("The result of nCr -> [%d]C[%d] = %d\n", n, r, ncr(n,r));
+
 }
 
-int ncr(int n, int r, int*** ptr){
-	printf("Bud here with value &d\n", (*ptr)[n][r]);
-	if (n == r || r == 0){
-		(*ptr)[n][r] = 1;
-		return 1;
+int ncr(int n, int r){
+	int** ptr = (int**)malloc((n + 1) * sizeof(int*));
+	for(int i = 0; i <= n; i++){
+		ptr[i] = (int*)malloc((r + 1)* sizeof(int));
 	}
-	else if (r == 1 || r == n - 1){
-		(*ptr)[n][r] == n;
-		return n;
-	}
-	else{
-		if((*ptr)[n][r] == -1){
-			(*ptr)[n][r] = ncr(n - 1, r - 1, &(**ptr)) + ncr(n - 1, r, &(**ptr));
+	for(int i = 0; i <= n; i++){
+		for(int j = 0; j <= i; j++){
+			if(j == 0 || i == j){
+				ptr[i][j] = 1;
+			}
+			else{
+				ptr[i][j] = ptr[i - 1][j - 1] + ptr[i - 1][j];
+			}
+			//printf("%d ", ptr[i][j]);
 		}
-		else{
-			return (*ptr)[n][r];
-		}
+		//printf("\n");
 	}
+	return ptr[n][r];
 }
