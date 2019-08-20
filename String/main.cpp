6d8523840a987f71ad1d4925e40c8fe166e9c254
kevin909cty <cheungkevinviola909@gmail.com>
2019-08-21 00:18:58 +0800

    further update on string

--- String/main.cpp
+++ String/main.cpp
@@ -35,7 +35,10 @@ class String{
 		void countVowel();
 		void countWord();
 		char* reverse();
-		void compareDifferent();
+		bool palindrome();
+		bool anagram(); // Check if two stings are equal size and using same letters
+		bool findDuplicate(); 
+		bool findDuplicate_hash(); 
 	};
 
 int main(){
@@ -49,7 +52,10 @@ int main(){
 	objString.countWord();
 	objString.reverse();
 	objString.displayString();
-
+	objString.palindrome();
+	objString.anagram();
+	objString.findDuplicate();
+	objString.findDuplicate_hash();
 	return 0;
 
 }
@@ -123,56 +129,100 @@ char* String::reverse(){
 	return instring;
 }
 
-void compareDifferent(){
-	char* testString = "kevin is not here";
-	int charHash[26] = {0};
-	int testcount = 1;
-	for(int i = 0; testString[i] != '\0'; i++){
-		testcount++;
+bool String::palindrome(){ // Check if two stings are equal size and using same letters
+	for(int i = 0, j = length - 2; i < (length - 1)/2; i++, j--){
+		if(instring[i] == instring[j]){
+			continue;
+		}
+		else{
+			cout << "\nThe string input is not a palindrome\n";
+			return false;
+		}
 	}
-	/* TO_BE_CONTINUE
-	if(testcount >= length){
-		for(int i = 0; testString[i] != '\n'; i++){
-			if(instring[i] == '\0'){
-				if((instring[i] >= 97 && instring[i] <= 122) || (testString[i] >= 97 && testString[i] <= 122)){
+	cout << "\nThe string input is a palindrome\n";
+	return true;
+}
 
-				}
-				else if(){
+bool String::anagram(){
+	char testString[] = "i am kevin";
+	int record[26] = {0};
+	for (int i = 0; instring[i] != '\0'; i++){
+		if(instring[i] >= 65 && instring[i] <= 90){
+			record[instring[i] + 32 - 97]++;
+			record[testString[i] - 97]--;
+		}
+		else if(instring[i] >= 97 && instring[i] <= 122 || instring[i] == 32){
+			record[instring[i] - 97]++;
+			record[testString[i] - 97]--;
+		}
+		else{
+			continue;
+		}
+	}
+	for(int j = 0; record[j] < 26; j++){
+		if(record[j] != 0){
+			cout << "\nThe string input is not an anagram\n";
+			return false;
+		}
+	}
+	cout << "\nThe string input is an anagram\n";
+	return true;
+}
 
-				}
-				else{
-					continue;
-				}
+bool String::findDuplicate(){
+	int counter = 0;
+	for(int i = 0; instring[i] != '\0'; i++){
+		for(int j = i + 1; instring[j] != '\0'; j++){
+			if(instring[i] == 32){
+				continue;
 			}
-			else{
-
+			if(instring[i] == instring[j]){
+				printf("\nThe character %c duplicated\n", instring[i]);
+				counter++;
 			}
 		}
+	}
+	if(counter != 0){
+		return true;
 	}
 	else{
-		for(int i = 0; instring[i] != '\n'; i++){
-			if(testString[i] == '\0'){
+		cout << "\nNo duplicate character\n";
+		return false;
+	}
+}
 
-			}
-			else{
-				
-			}
+bool String::findDuplicate_hash(){
+	int recordHash[26] = {0};
+	int counter = 0;
+	for(int i = 0; instring[i] != '\0'; i++){
+		if(instring[i] == 32){
+			continue;
+		}
+		if(recordHash[instring[i] - 97] == 1){
+			printf("\n[HASH] The character %c duplicated\n", instring[i]);
+			recordHash[instring[i] - 97]++;
+			counter++;
+		}
+		else if(recordHash[instring[i] - 97] > 1){
+			recordHash[instring[i] - 97]++;
+		}
+		else{
+			recordHash[instring[i] - 97]++;
 		}
 	}
-	*/
+	if(counter != 0){
+		return true;
+	}
+	else{
+		cout << "\n[HASH] No duplicate character\n";
+		return false;
+	}
 }
 
 		/*
-		void palindrome();
-		void anagram(); // Check if two stings are equal size and using same letters
-	
-		// Check duplicated letters in a string
-		void findDuplicate(); 
-		void findDuplicate_hash(); 
 		void findDuplicate_bitwise(); cls
-
 		
-		// Print out all possibles permutation of a sting
+		Print out all possibles permutation of a sting
 		void permutation_backtrack(); 
 		void permutation_recursion(); 
 		void permutation_();
