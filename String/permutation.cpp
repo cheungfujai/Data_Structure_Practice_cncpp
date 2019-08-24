05c745b296219efe13a7c9505b03969e08446362
kevin909cty <cheungkevinviola909@gmail.com>
2019-08-24 14:18:16 +0800

    new

---
+++ String/permutation.cpp
@@ -0,0 +1,22 @@
+/*
+============================================================================================================================
+About_this_code: 	 Revising string and character array with this self-exercise.
+Compiler: 		 GCC, GNU Compiler Collection, "g++ -std=c++0x permutation -o compilefile"
+Author: 		 Cheung Tsz Yin
+Github:			 kevin909cty
+Date:   		 24 / 08 / 2019
+Comment: 		 Please feel free to contact me if there are any mistake or code that 
+			 can be further improve. Thank you very much! ^_^ 
+Contact email: 	 	 cheungkevinviola909@gmail.com
+============================================================================================================================
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+
+
+}
